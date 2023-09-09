b709a7c9eaffb5ff2fd7e3699972e9a6423a70aa
LeanneTN <2321604632@qq.com>
2023-09-09 11:52:32 +0800

    pair alignment

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import utils.sequence_alignment.Hamming as Hamming
+import utils.sequence_alignment.SimilarityScore as SimilarityScore
 
 if __name__ == '__main__':
-    seq1 = "GGGCCGTTGGT"
-    seq2 = "GGACCGTTGAC"
-    hamming = Hamming.Hamming(seq1, seq2)
-    print(hamming.Hamming_distance())
+    seq1 = "ATGTTAT"
+    seq2 = "ATCGTAC"
+    similarity = SimilarityScore.SimilarityScore(seq1, seq2)
+    print(similarity.pair_distance())
--- utils/sequence_alignment/SimilarityScore.py
+++ utils/sequence_alignment/SimilarityScore.py
@@ -1,2 +1,45 @@
+import numpy as np
 
+
+class SimilarityScore:
+    def __init__(self, *seqs):
+        self.seqs = seqs
+
+    def length_check(self):
+        """
+        Check if all sequences have the same length
+        :return: boolean
+        """
+        length = len(self.seqs[0])
+        for seq in self.seqs:
+            if len(seq) != length:
+                return False
+        return True
+
+    def pair_distance(self, score='edit'):
+        """
+        Calculate the edit distance between two sequences
+        :return: int edit distance
+        """
+        len1 = len(self.seqs[0])
+        len2 = len(self.seqs[1])
+        dp = np.zeros((len1 + 1, len2 + 1))
+        for i in range(len1 + 1):
+            dp[i][0] = i
+        for j in range(len2 + 1):
+            dp[0][j] = j
+
+        reward = 1
+        penalty = 0
+        if score == 'edit':
+            pass
+        elif score == 'BLAST':
+            reward = 5
+            penalty = -4
+
+        for i in range(1, len1 + 1):
+            for j in range(1, len2 + 1):
+                delta = reward if self.seqs[0][i - 1] == self.seqs[1][j - 1] else penalty
+                dp[i][j] = min(dp[i - 1][j - 1] + delta, min(dp[i - 1][j] + 1, dp[i][j - 1] + 1))
+        return dp[len1][len2]
 
